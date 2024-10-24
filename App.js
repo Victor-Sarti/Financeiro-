460ea1b536af599cb355be96001e0ae23cb91da8
Victor-Sarti <sartivictor24@gmail.com>
2024-10-24 11:13:55 -0300

    uptade app

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 
-import Routes from './src/Routes/index';
+import Routes from './src/routes/index';
 
 export default function App(){
   return(
